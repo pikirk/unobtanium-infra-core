7e2e180a629c414573e6042dbec906013fca62d9
Paul Kirk <pikirk@Pauls-MacBook-Pro.local>
2025-12-15 15:39:47 -0800

    add resource desc

--- stacks/web/core-api-gateway/main.tf
+++ stacks/web/core-api-gateway/main.tf
@@ -1,5 +1,6 @@
 resource "aws_apigatewayv2_api" "http_api" {
   name          = "${local.env}-apigateway-http"
+  description = "API Gateway for ${local.env} environment"
   protocol_type = "HTTP"
 
   tags = merge(local.tags, {
@@ -9,6 +10,7 @@ resource "aws_apigatewayv2_api" "http_api" {
 resource "aws_apigatewayv2_stage" "deployment_stage" {
   api_id      = aws_apigatewayv2_api.http_api.id
   name        = local.env
+  description = "Deployed stage for ${local.env} environment"
   auto_deploy = true
 
   tags = merge(local.tags, {
@@ -16,6 +18,6 @@ resource "aws_apigatewayv2_stage" "deployment_stage" {
 }
 
 output "invoke_url" {
-  description = "Stage invoke URL"
+  description = "Deployed invoke URL for ${local.env} environment"
   value       = aws_apigatewayv2_stage.deployment_stage.invoke_url
 }
