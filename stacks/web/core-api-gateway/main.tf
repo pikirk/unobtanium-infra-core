d2f03f75fe303a7cf8af9f0fe18af19ccc4670fe
Paul Kirk <pikirk@gmail.com>
2025-12-15 15:54:19 -0800

    Configure bare bones gateway

---
+++ stacks/web/core-api-gateway/main.tf
@@ -0,0 +1,23 @@
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "${local.env}-apigateway-http"
+  description = "API Gateway for ${local.env} environment"
+  protocol_type = "HTTP"
+
+  tags = merge(local.tags, {
+  })
+}
+
+resource "aws_apigatewayv2_stage" "deployment_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = local.env
+  description = "Deployed stage for ${local.env} environment"
+  auto_deploy = true
+
+  tags = merge(local.tags, {
+  })
+}
+
+output "invoke_url" {
+  description = "Deployed invoke URL for API Gateway"
+  value       = aws_apigatewayv2_stage.deployment_stage.invoke_url
+}
