6f156e19d905ec7e10db8e98188a3c4281186fb0
Paul Kirk <pikirk@gmail.com>
2025-12-23 11:41:07 -0800

    enable cors policy

--- stacks/web/core-api-gateway/env/dev.tfvars
+++ stacks/web/core-api-gateway/env/dev.tfvars
@@ -1,1 +1,11 @@
-env = "dev"
+env                = "dev"
+cors_allow_origins = ["*"]
+cors_allow_methods = ["OPTIONS", "GET"]
+cors_allow_headers = [
+  "Content-Type",
+  "Authorization",
+  "x-amz-date",
+  "x-api-key",
+  "x-amz-security-token"
+]
+cors_max_age_seconds = 3600
--- stacks/web/core-api-gateway/variables.tf
+++ stacks/web/core-api-gateway/variables.tf
@@ -7,3 +7,36 @@ variable "env" {
     error_message = "env must be one of: dev, stage, prod."
   }
 }
+
+############################################
+# HTTP API CORS configuration
+############################################
+variable "cors_allow_origins" {
+  description = "Allowed origins for CORS (HTTP API). Use explicit domains in prod."
+  type        = list(string)
+  default     = ["*"]
+}
+
+variable "cors_allow_methods" {
+  description = "Allowed methods for CORS preflight."
+  type        = list(string)
+  default     = ["GET", "OPTIONS"]
+}
+
+variable "cors_allow_headers" {
+  description = "Allowed headers for CORS preflight."
+  type        = list(string)
+  default = [
+    "content-type",
+    "authorization",
+    "x-amz-date",
+    "x-api-key",
+    "x-amz-security-token"
+  ]
+}
+
+variable "cors_max_age_seconds" {
+  description = "How long browsers can cache preflight results."
+  type        = number
+  default     = 3600
+}
